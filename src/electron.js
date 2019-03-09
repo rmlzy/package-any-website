572f1d644815150fb6c3ae6ff9ff6f14e8275088
rmlzy <liuzhiyuan2018@gmail.com>
2019-03-09 18:48:41 +0800

    调整最小宽度为1250px

--- src/electron.js
+++ src/electron.js
@@ -3,16 +3,18 @@ const builderConfig = require('../config/builder');
 const customConfig = require('../config/customConfig');
 const MenuBuilder = require('./menu');
 
-const { productName } = builderConfig;
-const { appUrl } = customConfig;
+const {productName} = builderConfig;
+const {appUrl} = customConfig;
 
 const {app, BrowserWindow} = electron;
 let mainWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
-        width: 1024,
-        height: 768
+        width: 1250,
+        height: 768,
+        minHeight: 1250,
+        minWidth: 768
     });
 
     mainWindow.setTitle(productName);
